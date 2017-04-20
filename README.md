187f7db004d68fa2575f234390ef38fe48f3a140
josh <joshua@marinacci.org>
2017-04-19 20:45:02 -0700

    support exponents add multiplication and division and subtraction

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
+
+
+Support exponents. 
+
+Render output with button to switch to exponents vs digits vs with commas
+
+
+---------------
+
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
 
 Below you will find some information on how to perform common tasks.<br>
--- src/parser.js
+++ src/parser.js
@@ -7,13 +7,20 @@ var sem = grammar.createSemantics().addOperation('calc', {
 	integer: function(a) {
 		return parseInt(this.sourceString,10);
 	},
-	float: function(a,b,c) {
+	float: function(a,b,c,e) {
 		return parseFloat(this.sourceString);
 	},
 	hex:  function(a,b) {
 		return parseInt(this.sourceString);
 	},
-	AddExpr_plus: ((a,_,b) => a.calc()+b.calc())
+    exp: function(_,sign,exp) {
+        return parseFloat(exp);
+    },
+	AddExpr_plus: ((a,_,b) => a.calc()+b.calc()),
+	AddExpr_minus: ((a,_,b) => a.calc()- b.calc()),
+    MulExpr_multiply: ((a,_,b) => a.calc() * b.calc()),
+    MulExpr_divide: ((a,_,b) => a.calc() / b.calc()),
+
 });
 
 module.exports = {
