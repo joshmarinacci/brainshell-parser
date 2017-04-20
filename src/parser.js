e467459d096d0e8e79f01f09d57b36565d841fd8
josh <joshua@marinacci.org>
2017-04-19 18:52:51 -0700

    rewrite tests to be easier to specify, add addition

--- src/parser.js
+++ src/parser.js
@@ -2,8 +2,8 @@ var fs = require('fs');
 var ohm = require('ohm-js');
 var grammar = ohm.grammar(fs.readFileSync('src/parser.ohm'));
 
-var sem = grammar.createSemantics().addOperation('toJS', {
-	Number: (a) => a.toJS(),
+var sem = grammar.createSemantics().addOperation('calc', {
+	Number: (a) => a.calc(),
 	integer: function(a) {
 		return parseInt(this.sourceString,10);
 	},
@@ -12,14 +12,15 @@ var sem = grammar.createSemantics().addOperation('toJS', {
 	},
 	hex:  function(a,b) {
 		return parseInt(this.sourceString);
-	}
+	},
+	AddExpr_plus: ((a,_,b) => a.calc()+b.calc())
 });
 
 module.exports = {
    parseString: function(str) {
 	var m = grammar.match(str);
 	if(m.failed()) throw new Error("match failed");
-	var js = sem(m).toJS();
+	var js = sem(m).calc();
 	console.log("parsing",str, "->", js);
 	return js;
    }
--- tests/simple.js
+++ tests/simple.js
@@ -1,38 +1,23 @@
 var test = require('tape');
+require('tape-approximately')(test);
 var Parser = require('../src/parser.js');
 
-console.log("running a simple test");
-
-test('does testing work',(t)=>{
-	console.log("testing");
-	t.end();
-});
-
-test('parse a number', (t)=>{
-	var str = "50";
-	console.log("parsing the number",str);
-	var val = Parser.parseString(str);
-	t.equal(val,50);
-	t.end();
-});
-
-test('parse a float', (t) => {
-	t.equal(Parser.parseString('50.0'),50.0);
-	t.equal(Parser.parseString('1.8'),1.8);
-	t.end();
-});
-
-test('parse 42 in various formats',(t)=>{
-	t.equal(Parser.parseString('42'),42);
-	t.equal(Parser.parseString('42.42'),42.42);
-	t.equal(Parser.parseString('0x42'),0x42);
-    //compareFormat(t,"42",42,"decimal");
-    //compareFormat(t,"x42",42,"hex");
-    //compareFormat("0xFFCC88",0xFFCC88,'hex');
-    //compareFormat("42 as hex",42,"hex");
-    //compareFormat("42 as octal",42,"octal");
-    //compareFormat("42 as binary",42,"binary");
-    //compareFormat("0xBEEF as decimal",0xBEEF,'decimal');
-	t.end();
-});
-
+function tests(msg,arr) {
+	test(msg, (t)=>{
+		arr.forEach((tcase) => {
+			var str = tcase[0];
+			var ans = tcase[1];
+			t.approximately(Parser.parseString(str),ans,0.00001);
+		});
+		t.end();
+	});
+}
+tests('parsing 42 in different formats', [
+	['42',42],
+	['4.2',4.2],
+	['0x42',0x42]
+]);
+tests("simple math 2", [
+	['4+2',6],
+	['4.4+2.2',6.6]
+]);
