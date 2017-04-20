57e0b3302882445d405d7d0f485f6a960cdf4e1a
josh <joshua@marinacci.org>
2017-04-19 17:39:44 -0700

    added initial parser

--- src/parser.js
+++ src/parser.js
@@ -1,5 +1,20 @@
-module.exports = {
-	parseString: function(str) {
-		return 50;
+var fs = require('fs');
+var ohm = require('ohm-js');
+var grammar = ohm.grammar(fs.readFileSync('src/parser.ohm'));
+
+var sem = grammar.createSemantics().addOperation('toJS', {
+	Number: function(a) {
+		return parseInt(this.sourceString,10);
 	}
+});
+
+module.exports = {
+   parseString: function(str) {
+	console.log("parsing",str);
+	var m = grammar.match(str);
+	console.log('failed = ', m.failed());
+	var js = sem(m).toJS();
+	console.log('js = ', js);
+	return js;
+   }
 }
